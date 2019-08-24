3a0385d1a0694dfebb1f6c5d7c575a9fd1ee3358
Zsolt Molnár <mozb17@gmail.com>
2019-08-24 15:22:16 +0200

    Add jquery.datatables. Reorganise BundaleConfig.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,8 +8,11 @@ namespace InventoryNatific
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/ Scripts / bootstrap.js",
+                        "~/ Scripts/DataTables/jquery.datatables.js",
+                        "~/ Scripts/DataTables/bootstrap.datatables.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,11 +22,12 @@ namespace InventoryNatific
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-sandstone.css",
+                      "~/Content/Datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
