37e4ef7ded2a2ce25a1ad98ed3da9868509b0b44
Zsolt Molnár <mozb17@gmail.com>
2019-08-24 12:43:02 +0200

    Add Product to Models

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryNatific.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public double Weight { get; set; }
+    }
+}
