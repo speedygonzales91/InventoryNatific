03a80fd02e2abd328aa3040b79159a8f0d8d9d43
Zsolt Molnár <mozb17@gmail.com>
2019-08-24 20:46:57 +0200

    Add Product Save/Edit.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -26,11 +26,58 @@ namespace InventoryNatific.Controllers
             return View();
         }
 
+        // GET: Products/GetProducts
         public ActionResult GetProducts()
         {
             var products = _context.Products.ToList();
             return Json(products, JsonRequestBehavior.AllowGet);
 
+        }
+
+        // GET: Products/New
+        public ActionResult New()
+        {
+            var product = new Product();
+            return View("ProductSave", product);
+        }
+
+        // GET: Products/Edit/1
+        public ActionResult Edit(int id)
+        {
+            var product = _context.Products.Find(id);
+            return View("ProductSave", product);
+        }
+
+
+        public ActionResult Save(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                var inValidProduct = new Product()
+                {
+                    Name = product.Name,
+                    Weight = product.Weight,
+                    Price = product.Price,
+                    Description = product.Description
+                };
+                return View("New", inValidProduct);
+            }
+
+            if (product.Id == 0)
+            {
+                _context.Products.Add(product);
+            }
+            else
+            {
+                var productInDb = _context.Products.Single(p => p.Id == product.Id);
+                productInDb.Name = product.Name;
+                productInDb.Weight = product.Weight;
+                productInDb.Price = product.Price;
+                productInDb.Description = product.Description;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Products");
         }
     }
 }
