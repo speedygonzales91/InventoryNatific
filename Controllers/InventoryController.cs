b64edd0876561a1eef2e2ffe15e2b07dbd8ef7eb
Zsolt Molnár <mozb17@gmail.com>
2019-08-24 23:28:30 +0200

    Remove all unnecessary usings.

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -1,9 +1,6 @@
 ﻿using InventoryNatific.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Data.Entity;
-using System.Web;
 using System.Web.Mvc;
 using System.Net;
 using System.Web.Http;
--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,8 +1,5 @@
 ﻿using InventoryNatific.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace InventoryNatific.Controllers
